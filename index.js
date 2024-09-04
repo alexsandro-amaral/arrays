2d35cbb8bd386828f3e614e25919d6909b2c40f2
alexsandro-amaral <alex.maral16@gmail.com>
2024-09-04 20:11:40 -0300

    inicio dos metodos com array

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1 - criar uma lista vazia
+let listajogos = ['lost planet', 'crash 3','ace combat']
+
+let listaestrela = [{nome: 'angelo', idade: 12}, 'betegeuse', 'sirius', 'anilam', 12]
+
+// 2 - alterar todos os itens para maiusculo
+console.log(listajogos[2].toUpperCase());
+
+// 3 - alterar todos os lementos para maiusculo
+let pessoas = [
+    'AngeLo lustosa',
+    'Juan BenJamin',
+    'MAteus AraNHA'
+]
+
+/* pessoas.map((item) => console.log(item.toUpperCase()))
+
+// 3.2 - forEach se arrow function
+/* pessoas.forEach(function(item) {
+return console.log(item.toUpperCase());
+})*/
+
+// 4 - adicionar item no array
+
+
